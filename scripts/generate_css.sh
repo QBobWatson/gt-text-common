327e2edcfaa6175a34bcf6f53197940309be21a1
Joe Rabinoff <jrabinoff6@math.gatech.edu>
2017-08-11 14:50:13 -0400

    New build script

--- scripts/generate_css.sh
+++ scripts/generate_css.sh
@@ -3,7 +3,6 @@
 realpath=$(realpath "$0")
 script_dir=$(dirname "$realpath")
 cd "$script_dir/../.."
-cd lib/mathbook-assets
+cd mathbook-assets
 
 compass compile --time --force
-"$script_dir/copy_static.sh"
